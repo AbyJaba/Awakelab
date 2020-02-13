33e2ce2bf75eb1cc622f80c8754cf3265c37eb37
Gabriel <dojavajava@gmail.com>
2020-02-12 21:31:00 -0300

    Prueba 1

---
+++ src/clasesPoo/Datos.java
@@ -0,0 +1,5 @@
+package clasesPoo;
+
+public class Datos {
+
+}
---
+++ src/clasesPoo/Metodos.java
@@ -0,0 +1,9 @@
+package clasesPoo;
+
+public class Metodos {
+
+	public static void mensaje() {
+			   System.out.println("hola mundo");
+	}
+	
+}
